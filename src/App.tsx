702eddec69e85c40aab37b8a7d1d73ca09d751e7
Seidali_ <seitmambetovseidali@gmail.com>
2022-11-30 00:43:45 +0000

    create-component-Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+
 
 function App() {
     console.log("App rendering");
@@ -10,7 +12,7 @@ function App() {
             <Rating/>
             <Accordion/>
         </div>
-    );
+    )
 }
 
 function AppContent() {
@@ -42,36 +44,6 @@ function Star() {
     )
 }
 
-function Accordion() {
-    console.log("Accordion rendering");
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
 
-function AccordionTitle() {
-    console.log("AccordionTitle rendering");
-    return (
-        <div>
-            <h3>Menu</h3>
-        </div>
-    )
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering");
-    return (
-        <div>
-            <ul className="lists">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    )
-}
 
 export default App;
